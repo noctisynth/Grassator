6464bf201563bbb6c54928195604a9427cdb01d9
苏向夜 <fu050409@163.com>
2024-07-13 21:25:02 +0800

    feat(download): add a download demo

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -18,12 +18,34 @@ async fn get_file_size(url: &str) -> Result<models::FileSize, ()> {
     }
 }
 
+#[tauri::command]
+async fn download_file(
+    url: &str,
+    output: &str,
+    config: tauri::State<'_, state::Config>,
+) -> Result<models::DownloadResult, ()> {
+    match utils::download_file(url, output, &config.inner()).await {
+        Ok(_) => Ok(models::DownloadResult {
+            file_path: output.to_string(),
+            error: None,
+        }),
+        Err(e) => Ok(models::DownloadResult {
+            file_path: String::new(),
+            error: Some(e.to_string()),
+        }),
+    }
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .manage(state::Config::default())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![get_config, get_file_size])
+        .invoke_handler(tauri::generate_handler![
+            get_config,
+            get_file_size,
+            download_file
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src-tauri/src/state.rs
+++ src-tauri/src/state.rs
@@ -3,14 +3,14 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub(crate) struct Config {
     pub(crate) preset_file: String,
-    pub(crate) threads_count: u32,
+    pub(crate) num_threads: u32,
 }
 
 impl Default for Config {
     fn default() -> Self {
         Self {
             preset_file: String::from("presets/default.json"),
-            threads_count: 1,
+            num_threads: 7,
         }
     }
 }
