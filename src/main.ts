d2f83e4fabf2819e36cb75b64aa8b125363a0b09
苏向夜 <fu050409@163.com>
2024-07-13 14:30:27 +0800

    chore(lint): fix code style lint

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,36 @@
-import "virtual:uno.css";
-import "primeicons/primeicons.css";
+import 'virtual:uno.css';
+import 'primeicons/primeicons.css';
 
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
 
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
-import Ripple from "primevue/ripple";
-import Toast from "primevue/toast";
-import ToastService from "primevue/toastservice";
+import Ripple from 'primevue/ripple';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
 
-const app = createApp(App)
-const pinia = createPinia()
+const app = createApp(App);
+const pinia = createPinia();
 
 app.use(PrimeVue, {
-    ripple: true,
-    theme: {
-        preset: Aura,
-        options: {
-            prefix: 'p',
-            darkModeSelector: '.dark',
-            cssLayer: false
-        }
-    }
+  ripple: true,
+  theme: {
+    preset: Aura,
+    options: {
+      prefix: 'p',
+      darkModeSelector: '.dark',
+      cssLayer: false,
+    },
+  },
 });
 app.use(ToastService);
-app.directive("ripple", Ripple);
-app.component("Toast", Toast);
+app.directive('ripple', Ripple);
+app.component('Toast', Toast);
 
 app.use(pinia);
 app.use(router);
 
-app.mount("#app");
+app.mount('#app');
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,26 +2,26 @@ import {
   createRouter,
   createWebHashHistory,
   type RouteRecordRaw,
-} from "vue-router";
+} from 'vue-router';
 
 const views = import.meta.glob([
-  "../views/**/*.vue",
-  "../views/**/index.vue",
-  "../views/**/\\[*\\].vue",
+  '../views/**/*.vue',
+  '../views/**/index.vue',
+  '../views/**/\\[*\\].vue',
 ]);
 
 const routes: RouteRecordRaw[] = Object.entries(views).map(
   ([filePath, component]) => {
     let path = filePath
-      .replace(/^\.\.\/views\//, "")
-      .replace(/\.vue$/, "")
-      .replace(/^(.*)\/?index$/, "$1")
-      .replace(/\[(\w+)\]$/, ":$1");
-    path = "/" + path;
+      .replace(/^\.\.\/views\//, '')
+      .replace(/\.vue$/, '')
+      .replace(/^(.*)\/?index$/, '$1')
+      .replace(/\[(\w+)\]$/, ':$1');
+    path = '/' + path;
 
     return {
       path,
-      name: filePath.replace(/^\.\.\/views\//, ""),
+      name: filePath.replace(/^\.\.\/views\//, ''),
       component,
     } satisfies RouteRecordRaw;
   }
