624a4fa207d33fdeb9d2fd456c1e61f8a6113a58
YESWmeshade <392950974@qq.com>
2024-07-23 22:18:23 +0800

    feat(searchtext): add searchtext

--- src/views/index.vue
+++ src/views/index.vue
@@ -17,6 +17,7 @@ const menu = shallowReactive([
   },
 ]);
 const activeMenu = ref<number>(0);
+const searchtext = ref<string>('');
 
 const theme = ref(
   new Proxy(
@@ -73,7 +74,7 @@ onMounted(async () => {
             <InputIcon>
               <i class="pi pi-search"></i>
             </InputIcon>
-            <InputText size="small" placeholder="Search" disabled />
+            <InputText v-model="searchtext" size="small" placeholder="Search" />
           </IconField>
         </div>
 
